b5a9d2bb5df886cec7e0caccf650453a819f220a
David Flores Silva <dfloressi@unsa.edu.pe>
2021-06-09 18:52:05 -0500

    se creo la clase Ciudad

--- ciudades/models.py
+++ ciudades/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=200)
+    pais = models.CharField(max_length=50)
+    departamento = models.CharField(max_length=50)    
+    oferta = models.BooleanField()    
+    precio = models.PositiveIntegerField() 
