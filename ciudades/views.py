d9030b3bffd2fc27ae43acfb028aa81907ad322e
David Flores Silva <dfloressi@unsa.edu.pe>
2021-06-10 13:03:31 -0500

    cree nuevos objetos en el archivo views en la primera funcion, borre el contenido de los parrafos en el home.html para llenarlos con las variables que pasamos por el contexto de la funcion

--- ciudades/views.py
+++ ciudades/views.py
@@ -1,9 +1,33 @@
+from ciudades.models import Ciudad
 from django.shortcuts import render
 from django.http import request
+from .models import Ciudad
 
 # Create your views here.
 def paginaPrincipal(request):
-    return render(request, "home.html", {})
+    #crearemos objetos de la clase Ciudad
+    c1 = Ciudad()
+    c1.nombre = "Ciudad Blanca"
+    c1.pais = "Peru"
+    c1.departamento = "Arequipa"
+    c1.oferta = True
+    c1.precio = 500
+
+    c2 = Ciudad()
+    c2.nombre = "Brasilia"
+    c2.pais = "Brasil"
+    c2.departamento = "Brasilia"
+    c2.oferta = False
+    c2.precio = 600
+
+    c3 = Ciudad()
+    c3.nombre = "Uruguay"
+    c3.pais = "Uruguay"
+    c3.departamento = "Montevideo"
+    c3.oferta = True
+    c3.precio = 700
+
+    return render(request, "home.html", {"c1": c1, "c2": c2, "c3": c3})
 
 
 def ciudad1(request):
